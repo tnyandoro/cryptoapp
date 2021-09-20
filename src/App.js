b0db73b589dc667a9893ceb0c1c79b8cf37fb531
Tendai <tendai1@hotmail.com>
2021-09-20 21:50:30 +0200

    ceate navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       
     </div>
   )
