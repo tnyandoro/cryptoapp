63611cbe1bb8a1a1c0986e9bf3a5555956a0e59e
Tendai <tendai1@hotmail.com>
2021-09-20 22:16:49 +0200

    Add the navbar, logo and topography divs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, 
+  MoneyCollectOutlined, 
+  BulbOutlined, 
+  FundOutlined, 
+  MenuOutlined } from '@ant-design/icons'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="nav-container">
+      <div className="logo-contaier">
+        <Avatar />
+        <Typography.Title level={2} className="logo"></Typography.Title>
+      </div>
       
     </div>
   )
