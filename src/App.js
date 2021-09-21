6b9e91cea16b52bb64f5efd79669045f07c5f1f2
Tendai <tendai1@hotmail.com>
2021-09-22 00:26:04 +0200

    Add the homepage after fixing the footer

--- src/App.js
+++ src/App.js
@@ -34,19 +34,20 @@ const App = () => (
           </Switch>
         </div>
       </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+          Cryptoradical
+          {' '}
+          <br />
+          All Rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
     </div>
-    <div className="footer" />
-    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-      Cryptoradical
-      {' '}
-      <br />
-      All Rights reserved
-    </Typography.Title>
-    <Space>
-      <Link to="/">Home</Link>
-      <Link to="/">Home</Link>
-      <Link to="/">Home</Link>
-    </Space>
   </div>
 );
 
