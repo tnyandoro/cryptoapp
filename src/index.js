fb7e5e20ac29088fabdb4318efd6c2b31d0ee7b3
Tendai <tendai1@hotmail.com>
2021-09-22 00:59:32 +0200

    Add react redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import store from './app/store';
