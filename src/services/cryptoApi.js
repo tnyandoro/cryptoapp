46c3df322519f21168bd0e4d87f131417cf8e2ee
Tendai <tendai1@hotmail.com>
2021-09-22 00:55:52 +0200

    Add the configurestore

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': '6803afc805mshe7979e6bc5314e9p100bf5jsn54ac9e3aae4a',
+//   },
+// };
