b7b82059e8c8a8ca732abed80e438e6bdcd54563
Tendai <tendai1@hotmail.com>
2021-09-21 16:56:13 +0200

    Add new changes

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as CryptoDetails } from './CryptoDetails';
 export { default as Exchanges } from './Exchanges';
 export { default as Homepage } from './Homepage';
 export { default as News } from './News';
+
+e
