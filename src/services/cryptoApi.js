8af6abec75de1aec57ce04eb9363b632675d898c
Tendai <tendai1@hotmail.com>
2021-09-22 01:07:43 +0200

    Set the base url and the headers for the api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,5 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 
+const cryptoApiHeaders {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '6803afc805mshe7979e6bc5314e9p100bf5jsn54ac9e3aae4a',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 // const options = {
 //   method: 'GET',
 //   url: 'https://coinranking1.p.rapidapi.com/exchanges',
