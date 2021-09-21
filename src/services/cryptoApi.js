8563568a411f89934a8ef593ebf9248e2e587f5e
Tendai <tendai1@hotmail.com>
2021-09-22 01:19:49 +0200

    create the create reaquest variable

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,16 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 
-const cryptoApiHeaders {
+const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
   'x-rapidapi-key': '6803afc805mshe7979e6bc5314e9p100bf5jsn54ac9e3aae4a',
-}
+};
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//   headers: {
-//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//     'x-rapidapi-key': '6803afc805mshe7979e6bc5314e9p100bf5jsn54ac9e3aae4a',
-//   },
-// };
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+default export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder)=>({
+    getCryptos: builder.query({
+
+    })
+  })
+});
