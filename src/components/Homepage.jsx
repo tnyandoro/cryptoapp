cceb8642356c6dc3608bac70408db54159cddf32
Tendai <tendai1@hotmail.com>
2021-09-22 00:29:12 +0200

    Add imports of dependences from react and antd

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => (
   <div>
