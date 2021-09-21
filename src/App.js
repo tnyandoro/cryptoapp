b936a137336f59c93104b31020ef7d7461f557f3
Tendai Nyandoro <tendai1@hotmail.com>
2021-09-21 14:13:04 +0200

    Add App css file

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import { Navbar } from './components';
+import './App.css';
+
 const App = () => {
-  return (
-    <div className="app">
-      <div className="navbar">
+        return (
+        <div className="app">
+          <div className="navbar">
+            <Navbar />
+          </div>
+          <div className="main" />
+          <div className="footer" />
+        </div>
+      ); 
+      }
 
-      </div>
-      <div className="main">
-
-      </div>
-      <div className="footer">
-        
-      </div>
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
+import {
+  Button, Menu, Typography, Avatar,
+} from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, 
-  MoneyCollectOutlined, 
-  BulbOutlined, 
-  FundOutlined, 
-  MenuOutlined } from '@ant-design/icons'
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png';
 
-const Navbar = () => {
-  return (
-    <div className="nav-container">
-      <div className="logo-contaier">
-        <Avatar />
-        <Typography.Title level={2} className="logo">
-          <Link to="/"> Radical Crypto</Link>
-        </Typography.Title>
-        <Button className="menu-control-contaiber">
-          
-        </Button>
-      </div>
-      
+const Navbar = () => (
+  <div className="nav-container">
+    <div className="logo-container">
+      <Avatar src={icon} size="large" />
+      <Typography.Title level={2} className="logo">
+        <Link to="/"> Radical Crypto</Link>
+      </Typography.Title>
     </div>
-  )
-}
 
-export default Navbar
+  </div>
+);
+
+export default Navbar;
